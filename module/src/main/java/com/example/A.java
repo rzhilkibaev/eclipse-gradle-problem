6693abd2337ec961d3872e2f078e4d5a2059e865
Renat <rzhilkibaev@gmail.com>
2016-08-17 22:50:25 -0700

    blah

---
+++ module/src/main/java/com/example/A.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import org.slf4j.*;
+
+public class A {
+
+    public Logger log = LoggerFactory.getLogger(this.getClass());
+}
---
+++ src/accTest/java/com/example/ATest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.slf4j.*;
+import com.google.common.collect.ImmutableMap;
+
+public class ATest {
+
+    public Logger log = LoggerFactory.getLogger(this.getClass());
+    public ImmutableMap<String, Object> map;
+}
